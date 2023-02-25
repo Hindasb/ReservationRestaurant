0ec15f8988d4bae2a2be52c4c3d7b421a1ab3b49
Hindasb <123954679+Hindasb@users.noreply.github.com>
2023-02-25 14:17:31 +0000

    Revert "modification login design"

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\DELL\Desktop\Nouveau dossier (8)\ReservationRestaurant\env
+set VIRTUAL_ENV=C:\Users\Hinda Bekaye\Desktop\restau\env
 
 if not defined PROMPT set PROMPT=$P$G
 
